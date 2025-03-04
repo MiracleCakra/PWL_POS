85f48bdac733466539dca04d68545a9bc3006e68
Cakra Wangsa M.A.W <ckrawangsa@gmail.com>
2025-03-04 08:49:58 +0700

    Praktikum 1 - $fillable

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -3,33 +3,25 @@
 namespace App\Http\Controllers;
 
 use App\Models\UserModel;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
+
 
 class UserController extends Controller
 {
     public function index()
     {
-        // //tambah data user dengan Eloquent Model
-        // $data = [
-        //     'username' => 'customer-1',
-        //     'nama' => 'Pelanggan',
-        //     'password' => Hash::make('12345'),
-        //     'level_id' => 4
-        // ];
+        $data = [
+            'level_id' => 2,
+            'username' => 'manager_tiga',
+            'nama' => 'Manager 3',
+            'password' => Hash::make('12345')
+        ];
+        UserModel::create($data);
 
-        // UserModel::insert($data); //tambahkan data ke tabel m_user
-
-        //tambah data dengan Eloquent Model
-          $data = [
-              'nama' => 'Pelanggan',
-          ];
-
-          UserModel::where('username', 'customer-1')->update($data);//update data user
-
-
-        //mencoba akses model UserModel
-        $user = UserModel::all(); //Mengambil semua data dari tabel m_user
+        $user = UserModel::all();
         return view('user', ['data' => $user]);
     }
 }
+
